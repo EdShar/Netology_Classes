2c0c42f42440668cc7d8b9f15dc1510c02e4683a
EdShar <dvjtarantul@gmail.com>
2023-05-30 21:47:48 +0300

    Add rate_lector method

--- attributes.py
+++ attributes.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lector(self, lector, course, grade):
+        if isinstance(lector,
+                      Lecturer) and course in self.courses_in_progress and course in lector.courses_attached:
+            if course in lector.grades:
+                lector.grades[course] += [grade]
+            else:
+                lector.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -18,16 +28,8 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-
-    def rate_hw(self, student, course, grade):
-        if isinstance(student,
-                      Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+        self.grades = {}
+        self.courses_attached = []
 
 
 class Reviewer(Mentor):
@@ -43,7 +45,6 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
 
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.courses_in_progress += ['Python']
