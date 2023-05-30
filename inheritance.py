7f2c4bfe9ee154ded5f32bab494db49ba1412471
EdShar <dvjtarantul@gmail.com>
2023-05-30 21:22:55 +0300

    Add comments to print()

--- inheritance.py
+++ inheritance.py
@@ -34,15 +34,14 @@ class Reviewer(Mentor):
         super().__init__(name, surname)
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-best_student.rate_hw(best_student, 'Python', 10)
-best_student.rate_hw(best_student, 'Python', 10)
-best_student.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
-
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+#
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+#
+# best_student.rate_hw(best_student, 'Python', 10)
+# best_student.rate_hw(best_student, 'Python', 10)
+# best_student.rate_hw(best_student, 'Python', 10)
+#
+# print(best_student.grades)
